12b82adce4c4cbcf9975aecaf8d0c600870c1317
charukaonline <nuwanthakonline@gmail.com>
2023-12-27 15:21:00 +0530

    login php update

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,8 +3,8 @@
 include_once 'dbh.inc.php';
 
 if (isset($_POST['submit'])) {
-    $uname = $_POST['uname'];
-    $pass = $_POST['pass'];
+    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+    $pass = mysqli_real_escape_string($conn, $_POST['pass']);
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
